1b5952d97f0fa2283aa909da87581d381154305a
Jean198 <jeandedieu.niyigaba@gmail.com>
2023-03-28 10:12:58 +0300

    login functionalities

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,13 +2,16 @@ import React from 'react';
 import Humburger from '../humburger/Humburger';
 import { ImUser } from 'react-icons/im';
 import './header.css';
+import { useSelector } from 'react-redux';
+import { selectUserInfo } from '../../redux/features/auth/authSlice';
 
 const Header = () => {
+  const { name } = useSelector(selectUserInfo);
   return (
     <div className='header'>
       <div className='title'>
         <ImUser size='25' />
-        <div className='user-container'>Jean</div>
+        <div className='user-container'>{name}</div>
       </div>
       <Humburger />
     </div>
--- src/pages/auth/login/Login.js
+++ src/pages/auth/login/Login.js
@@ -1,23 +1,82 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './login.css';
-import { AiFillLock, AiOutlineLoading3Quarters } from 'react-icons/ai';
+import { AiFillLock } from 'react-icons/ai';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { loginUser } from '../../../services/authService';
+
+import { setLogin, setName } from '../../../redux/features/auth/authSlice';
+import Spinner from '../../../components/spinner/Spinner';
+const initialState = {
+  username: '',
+  password: '',
+};
+
+const toastPosition = {
+  position: toast.POSITION.TOP_CENTER,
+  autoClose: 2000,
+};
 
 const Login = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const [formData, setFormData] = useState(initialState);
+  const { username, password } = formData;
+
+  const handleInputChange = (e) => {
+    console.log(e.target.value);
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const login = async (e) => {
+    e.preventDefault();
+
+    if (!username || !password) {
+      return toast.error('All fields are required', toastPosition);
+    }
+
+    const userData = {
+      username,
+      password,
+    };
+
+    setIsLoading(true);
+
+    try {
+      const data = await loginUser(userData);
+      dispatch(setLogin(true));
+      dispatch(setName(data.name));
+      navigate('/dashboard');
+    } catch (error) {
+      setIsLoading(false);
+    }
+  };
   return (
     <>
       <div className='form-container'>
+        {isLoading && <Spinner />}
         <div className='auth-form'>
           <AiFillLock size='30' className='mb-4 ' />
 
-          <form>
+          <form onSubmit={login}>
             <input
               type='text'
               placeholder='Username'
               name='username'
-              value=''
+              value={username}
+              onChange={handleInputChange}
             />
-            <input type='password' placeholder='Password' name='password' />
+            <input
+              type='password'
+              placeholder='Password'
+              name='password'
+              value={password}
+              onChange={handleInputChange}
+            />
             <button>Login</button>
             <p className='message'>
               Back to <Link to='/'>Homepage</Link>
